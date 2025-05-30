c86c903ee6ce27aac88192df401e10e2fc90f842
damian-pebe <damian.pebe@gmail.com>
2025-05-30 00:15:48 -0600

    fixed responsive UI and some minor changes

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -14,22 +14,22 @@ export default function Hero() {
           className="object-cover opacity-90"
         />
       </div>
-      <div className="relative z-10 flex flex-col h-full w-full py-20 px-16 justify-between text-center md:text-start text-white font-careny">
-        <p className="text-3xl md:text-5xl h-full tracking-wide">
-          Fancy<sup>®</sup>Microblading
+      <div className="relative z-10 flex flex-col h-full w-full pt-10 pb-48 md:py-20 px-5 md:px-16 justify-between text-start text-white font-careny">
+        <p className="text-6xl md:text-5xl h-full tracking-wide">
+          Fancy<sup>®</sup> <br className="block md:hidden"/>  Microblading
         </p>
-        <div className="flex justify-between items-center md:mb-10 md:ml-5">
-          <p className="text-5xl md:text-7xl tracking-wide w-full">
+        <div className="flex flex-col md:flex-row justify-between h-full md:h-min items-center mb-10 md:ml-5">
+          <p className="text-3xl md:text-7xl text-center md:text-start tracking-wide w-full">
             Arte en cada trazo, elegancia en tu rostro
           </p>
-          <div className="w-[70%] flex flex-col justify-center items-start">
-            <p className="text-base md:text-lg tracking-wide w-full font-poppins mb-5">
+          <div className="w-full md:w-[70%] flex flex-col justify-center items-start">
+            <p className="hidden md:block text-base md:text-lg tracking-wide w-full font-poppins mb-5">
               Donde cada trazo revela tu esencia. Cejas y pestañas que realzan tu belleza natural
             </p>
 
             <button
               onClick={() => (window.location.href = "/contact")}
-              className="group flex items-center gap-2 border border-black/30 rounded-full ml-5 px-4 py-2 text-base font-xl transition hover:-translate-y-1 duration-1000"
+              className="group flex items-center gap-2 border border-black/30 rounded-full ml-12 md:ml-5 px-4 py-2 text-3xl transition hover:-translate-y-1 duration-1000"
             >
               CONTACTANOS{" "}
               <ArrowUpRightIcon
