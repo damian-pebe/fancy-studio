eb4486662b2ab97c33150a62089f0c6669c46c89
damian-pebe <damian.pebe@gmail.com>
2025-06-14 15:31:53 -0600

    removed unused var

--- src/app/agendar/components/Dates.tsx
+++ src/app/agendar/components/Dates.tsx
@@ -32,10 +32,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 
